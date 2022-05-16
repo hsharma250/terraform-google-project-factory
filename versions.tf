1d7875f1ce73881a82dffb753f94e1a58d2de84a
Harsh Sharma <harshbsharma@google.com>
2022-05-16 19:48:27 +0530

    Add Provider blocks

--- versions.tf
+++ versions.tf
@@ -43,11 +43,3 @@ provider "google" {
     region      = "us-central1"
     zone        = "us-central1-c"
   }
-  provider "deviavir/gsuite" {
-  oauth_scopes = [
-    "https://www.googleapis.com/auth/admin.directory.group",
-    "https://www.googleapis.com/auth/apps.groups.settings",
-    "https://www.googleapis.com/auth/admin.directory.user",
-    "https://www.googleapis.com/auth/admin.directory.userschema",
-  ]
-  }
